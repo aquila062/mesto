da8ad33bac58f521577dfe7d9b2eb3704bcf3c30
aquila062 <120404495+aquila062@users.noreply.github.com>
2023-06-13 21:12:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ let popUpName = document.querySelector('.form__input_value_name');
 let popUpActivity = document.querySelector('.form__input_value_position');
 
 openPopUp.addEventListener("click", () => {
-  popUp.classList.add('popap__active');
+  popUp.classList.add('popap__profile_visible');
   popUpName.value = profileName.textContent;
   popUpActivity.value = profileactivity.textContent;
 
 })
 
 closePopUp.addEventListener('click', () => {
-popUp.classList.remove('popap__active');
+popUp.classList.remove('popap__profile_visible');
 
 })
 
@@ -23,7 +23,7 @@ function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = popUpName.value;
   profileactivity.textContent = popUpActivity.value;
-  popUp.classList.remove('popap__active');
+  popUp.classList.remove('popap__profile_visible');
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
